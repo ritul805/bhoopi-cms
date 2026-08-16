e5e4c9104d9b85f81b0ca6c68a1a1c6a217ab75b
Ritul Singh <ritulsingh@Rituls-MacBook-Air.local>
2026-08-17 02:15:53 +0530

    Fix story totals drift and make category updates atomic

--- query.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({ connectionString: 'postgresql://postgres:Naanikikahani%4012345@db.ozdvhjcumeujfxodiawc.supabase.co:5432/postgres' });
-client.connect();
-client.query("SELECT tablename FROM pg_tables WHERE schemaname='public';", (err, res) => {
-  console.log(res.rows.map(r => r.tablename));
-  client.end();
-});
